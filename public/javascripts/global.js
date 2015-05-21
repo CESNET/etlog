616c63e1eda623ae5786561e54c7f19d1d583565
Václav Mach <vac.mach@sh.cvut.cz>
2015-05-21 12:01:59 +0000

    pridana vizualni validace uzivatelskeho jmena

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -171,91 +171,51 @@ function validateMac()
 
 
 // --------------------------------------------------------------------------------------
-  // form validation
-  // taken from http://formvalidation.io/examples/adding-dynamic-field/
+// taken from http://formvalidation.io
+// --------------------------------------------------------------------------------------
+// custom validation
 
-    // The maximum number of options
-    var MAX_OPTIONS = 5;
+ FormValidation.Validator.username = {
+        validate: function(validator, $field, options) {
+            var value = $field.val();
+ 
+            if (value === '')
+                return true;
 
-    $('#surveyForm')
-        .formValidation({
-            framework: 'bootstrap',
-            icon: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
-            },
-            fields: {
-                question: {
-                    validators: {
-                        notEmpty: {
-                            message: 'The question required and cannot be empty'
-                        }
+            // TODO - jak muze vypadat username ? specialni znaky ve jmenu - tecky, .... ?
+
+            var matcher = new RegExp("^[a-zA-Z0-9]+@([a-zA-Z]+\\.){1,}[a-zA-Z]+$", "i");
+            var found = matcher.test(value);
+            
+            if(found)
+                return true;
+
+            return {
+                valid: false,
+                message: 'Není platné uživatelské jméno.'
+            }
+        }
+    };
+// --------------------------------------------------------------------------------------
+
+    $('#main_form').formValidation({
+        framework: 'bootstrap',
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            username: {
+                validators: {
+                    username: {
                     }
-                },
-                'option[]': {
-                    validators: {
-                        notEmpty: {
-                            message: 'The option required and cannot be empty'
-                        },
-                        stringLength: {
-                            max: 100,
-                            message: 'The option must be less than 100 characters long'
-                        }
-                    }
                 }
             }
-        })
-
-        /*
-        // Add button click handler
-        .on('click', '.addButton', function() {
-            var $template = $('#optionTemplate'),
-                $clone    = $template
-                                .clone()
-                                .removeClass('hide')
-                                .removeAttr('id')
-                                .insertBefore($template),
-                $option   = $clone.find('[name="option[]"]');
-
-            // Add new field
-            $('#surveyForm').formValidation('addField', $option);
-        })
-
-        // Remove button click handler
-        .on('click', '.removeButton', function() {
-            var $row    = $(this).parents('.form-group'),
-                $option = $row.find('[name="option[]"]');
-
-            // Remove element containing the option
-            $row.remove();
-
-            // Remove field
-            $('#surveyForm').formValidation('removeField', $option);
-        })
-
-        // Called after adding new field
-        .on('added.field.fv', function(e, data) {
-            // data.field   --> The field name
-            // data.element --> The new field element
-            // data.options --> The new field options
-
-            if (data.field === 'option[]') {
-                if ($('#surveyForm').find(':visible[name="option[]"]').length >= MAX_OPTIONS) {
-                    $('#surveyForm').find('.addButton').attr('disabled', 'disabled');
-                }
-            }
-        })
-
-        // Called after removing the field
-        .on('removed.field.fv', function(e, data) {
-           if (data.field === 'option[]') {
-                if ($('#surveyForm').find(':visible[name="option[]"]').length < MAX_OPTIONS) {
-                    $('#surveyForm').find('.addButton').removeAttr('disabled');
-                }
-            }
-        });
-        */
-
+        }
+    });
 });
+
+
+
 
