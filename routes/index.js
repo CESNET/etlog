83aa1a55b28b1ffa49d809050ccaeddf78043cf4
Václav Mach <vac.mach@sh.cvut.cz>
2016-03-28 16:30:35 +0000

    pridany debug vypisy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  
+  //console.log(req);
+  console.log(req.server);
+  //console.log(req.query);   // debug
+  //console.log(req.headers);   // debug
+  //console.log(req.headers.cookie);
+  //console.log(typeof(req.headers.cookie));
+
+  //var url = require('url');
+  //var url_parts = url.parse(req.url, true);
+  //var query = url_parts.query;
+
+  //console.log(url_parts);
+  //console.log(query);
+
+
   res.render('index', { title: 'Ukazkove rozhrani pro vyhledavani nad radius logy' });
+
+  //var color = req.param('color');
+
 });
 
 module.exports = router;
