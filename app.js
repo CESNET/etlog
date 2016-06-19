843fa0df90f4ff8cf4d303333774edaa4bb03645
Václav Mach <vac.mach@sh.cvut.cz>
2016-06-19 19:09:47 +0000

    working setup with testing idp

--- app.js
+++ app.js
@@ -132,8 +132,9 @@ app.use(function(err, req, res, next) {
 
 // -----------------------------------------------------------
 
-var pvk = fs.readFileSync('/etc/shibboleth/sp-key_copy.pem', 'utf-8');
+var pvk = fs.readFileSync('/etc/shibboleth/sp-key.pem', 'utf-8');
 var cert = fs.readFileSync('/etc/shibboleth/sp-cert.pem', 'utf-8');
+var idpcert = fs.readFileSync('/etc/shibboleth/idp-cert.pem', 'utf-8');
 
 console.log("debug");
 console.log(cert);
@@ -150,9 +151,14 @@ var str = new SamlStrategy(
     protocol: 'https://',
     //generateServiceProviderMetadata: '/test',
     //cert: 'MIIFSDCCBDCgAwIBAgIQBXyYdVS7UxZI1dayu2ZXbDANBgkqhkiG9w0BAQsFADBkMQswCQYDVQQGEwJOTDEWMBQGA1UECBMNTm9vcmQtSG9sbGFuZDESMBAGA1UEBxMJQW1zdGVyZGFtMQ8wDQYDVQQKEwZURVJFTkExGDAWBgNVBAMTD1RFUkVOQSBTU0wgQ0EgMzAeFw0xNTExMTMwMDAwMDBaFw0xODExMjExMjAwMDBaMIGOMQswCQYDVQQGEwJDWjEdMBsGA1UECAwUSGxhdm7DrSBtxJtzdG8gUHJhaGExEDAOBgNVBAcTB1ByYWhhIDYxMzAxBgNVBAoMKsSMZXNrw6kgdnlzb2vDqSB1xI1lbsOtIHRlY2huaWNrw6kgdiBQcmF6ZTEZMBcGA1UEAxMQaWRwMi5jaXYuY3Z1dC5jejCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKFi5O/MIGOQ7k41iOHwDzkOIJL/0F5TZyR6sTBSLWTOlTo5JIeaWj4vcL6Vww6kbnV9uQci/8eEOSrnI9DHrfA59vjgcH67PE1EzEhgCaCfrcNT1RnLBsb0OgyXay9sfr/Ylq0UORDTyPNSp8zDOPpwzvcz5dmfQ4uOOP1R6H5h1BPJ0MlRaTtnuIaNQ71nUHmy0UWQc2dJTqf0xEXUSzbr28LD9TlaJjm9oPdnwPxPGUvc5YjxAe5ElH179C/rIkxaz0313LwOrYWEdNn0KNJ6mRkD4Xcj0MiJVD201Gjw6nSWnXLvfqOPxpv+vfKpnGnAUzBaMySV0spSVkU/iUECAwEAAaOCAckwggHFMB8GA1UdIwQYMBaAFGf9iCAUJ5jHCdIlGbvpURFjdVBiMB0GA1UdDgQWBBQ5zJX+DRBUzFYzzJTV7wxCcFZTOTAbBgNVHREEFDASghBpZHAyLmNpdi5jdnV0LmN6MA4GA1UdDwEB/wQEAwIFoDAdBgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwawYDVR0fBGQwYjAvoC2gK4YpaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL1RFUkVOQVNTTENBMy5jcmwwL6AtoCuGKWh0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9URVJFTkFTU0xDQTMuY3JsMEwGA1UdIARFMEMwNwYJYIZIAYb9bAEBMCowKAYIKwYBBQUHAgEWHGh0dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9DUFMwCAYGZ4EMAQICMG4GCCsGAQUFBwEBBGIwYDAkBggrBgEFBQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tMDgGCCsGAQUFBzAChixodHRwOi8vY2FjZXJ0cy5kaWdpY2VydC5jb20vVEVSRU5BU1NMQ0EzLmNydDAMBgNVHRMBAf8EAjAAMA0GCSqGSIb3DQEBCwUAA4IBAQAEdk6PHL6Vy1eHvc76Rvsdf1eeZYgQhC83Mu3Jl7YdJ3Gn0e9/Ae2uFUI3ojr0OUEqyLNyQyeDENJQ0bmn8N10sXxSSx4kTXe+FXjpCk/voBuAwIkm1e00/xmgfWLq40FVmZTeA4+h8NYs+ZVSDvLlBnsSj9urq61ARMT3jGlNeTzC0V+6CE9SizLKMJ1c5/jNzGM0V9zEp+fa293vqzGiIWE+Jk982d31nGCZR1WdrPcjGd2s3oEPc09v+pn5TTdHEeo+ANYmEJCunBeqEsRfmPxWNVpqZB+YKKqbDRGVQpSJUN8ck7lFolqJBnncLaNXBJP3EL3PvAXtdbMpcuHB',       // certifikat idp2.civ.cvut.cz
+    
+    // zatim nechat byt
     decryptionPvk: pvk,
-    privateCert: cert,
-    cert: cert
+    //privateCert: cert,
+    
+    identifierFormat : 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+    
+    cert: idpcert
     //privateCert: ''
 
 
@@ -160,13 +166,25 @@ var str = new SamlStrategy(
 
   },
   function(profile, done) {         // TODO
-    findByEmail(profile.email, function(err, user) {
-      if (err) {
-        return done(err);
-      }
-      return done(null, user);
+    console.log("autentizace OK");
+
+    // tady je treba se dostat k obsahu assertion - informace o prihlasenem uzivateli
+    console.log(profile);
+    console.log(profile.getAssertionXml());
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log(done());
+
+    //render("OK")
+    //findByEmail(profile.email, function(err, user) {
+    //  if (err) {
+    //    return done(err);
+    //  }
+    //  return done(null, user);
     });
-  })
+  //})
 
 
 passport.use(str);
