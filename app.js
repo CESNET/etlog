64a0ebc10bc59186dbff57c80eed887516272413
Václav Mach <vac.mach@sh.cvut.cz>
2016-08-14 15:30:39 +0000

    improve error logging

--- app.js
+++ app.js
@@ -84,6 +84,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    console.error(err);         // log error to console
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
