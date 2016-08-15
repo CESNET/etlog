d0eb88d24feb24f6e15990cccf6298eeb24f37ac
Václav Mach <vac.mach@sh.cvut.cz>
2016-08-15 18:40:42 +0000

    modify routes

--- routes.js
+++ routes.js
@@ -2,12 +2,15 @@ module.exports = function(app) {
   // set up routes
   app.use('/', require('./routes/index'));                      // empty title page
   app.use('/angular_test', require('./routes/angular_test'));   // angular test page
-  app.use('/search', require('./routes/search'));               // generic search
-  app.use('/mac', require('./routes/mac'));                     // generic mac address search
-  app.use('/roaming', require('./routes/roaming'));             // roaming 1 // TODO
+  app.use('/search/generic', require('./routes/search'));               // generic search
+  app.use('/search/mac', require('./routes/mac'));                     // generic mac address search
+  app.use('/search/roaming', require('./routes/roaming'));             // roaming 1 // TODO
   app.use('/inst_roaming', require('./routes/inst_roaming'));   // roaming 2 // TODO
-  app.use('/failed_logins', require('./routes/failed_logins')); // TODO
-  app.use('/failed_logins_search', require('./routes/failed_logins_search'));   // TODO
+  app.use('/search/failed_logins', require('./routes/failed_logins')); // TODO
+  app.use('/searach/failed_logins_search', require('./routes/failed_logins_search'));   // TODO
+
+  // TODO
+  // app.use('/stats', require('./routes/stats'));   // TODO
 
   // saml test
   app.use('/test', require('./routes/saml_test'));
