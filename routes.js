07585ddd5833bfd864b9625c6a52cb77e8c22bde
Václav Mach <vac.mach@sh.cvut.cz>
2016-08-15 19:40:19 +0000

    use meaningful route names and locations

--- routes.js
+++ routes.js
@@ -1,13 +1,13 @@
 module.exports = function(app) {
   // set up routes
-  app.use('/', require('./routes/index'));                      // empty title page
-  app.use('/angular_test', require('./routes/angular_test'));   // angular test page
-  app.use('/search/generic', require('./routes/search'));               // generic search
-  app.use('/search/mac', require('./routes/mac'));                     // generic mac address search
-  app.use('/search/roaming', require('./routes/roaming'));             // roaming 1 // TODO
-  app.use('/search/inst_roaming', require('./routes/inst_roaming'));   // roaming 2 // TODO
-  app.use('/search/failed_logins', require('./routes/failed_logins')); // TODO
-  app.use('/search/failed_logins_search', require('./routes/failed_logins_search'));   // TODO
+  app.use('/', require('./routes/index'));                                              // empty title page
+  app.use('/angular_test', require('./routes/angular_test'));                           // angular test page
+  app.use('/search/generic', require('./routes/search'));                               // generic search
+  app.use('/search/mac', require('./routes/mac'));                                      // generic mac address count > 2 search
+  app.use('/search/roaming/most_provided', require('./routes/roaming_most_provided'));  // generic search of institutions most providing roaming
+  app.use('/search/roaming/most_used', require('./routes/roaming_most_used'));          // generic search of institutions most using roaming
+  app.use('/search/failed_logins', require('./routes/failed_logins'));                  // TODO
+  app.use('/search/failed_logins_search', require('./routes/failed_logins_search'));    // TODO
 
   // TODO
   // app.use('/stats', require('./routes/stats'));   // TODO
