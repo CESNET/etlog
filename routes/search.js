b6c4e0a5bc447fe010212cce52e067ea9716fe35
Václav Mach <vac.mach@sh.cvut.cz>
2016-08-16 14:47:11 +0000

    get data from backend using angular

--- routes/search.js
+++ routes/search.js
@@ -2,46 +2,69 @@ var express = require('express');
 var router = express.Router();
 // --------------------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  res.render('search', { title: 'Ukazkove rozhrani pro vyhledavani nad radius logy' });
+  // if query string empty
+  if (Object.keys(req.query).length === 0) {
+    console.log("query string empty");
+    res.render('search', { title: 'Ukazkove rozhrani pro vyhledavani nad radius logy' });
+  }
+  else {
+    console.log("query string not empty");
+    search(req, res, respond);
+    //res.render('search', { title: 'Ukazkove rozhrani pro vyhledavani nad radius logy' });
+  }
+  // else
+  // angular -> request na stejnou url bez reloadu ?
+  // po odeslani formu nastaveni query stringu
+  // query string na zaklade obsahu formu
+  // http://stackoverflow.com/questions/17491054/append-url-parameters-dynamically-angularjs
+  //
+  //
+  //
+
 });
 // --------------------------------------------------------------------------------------
-router.post('/', function(req, res, next) {
-  search(req, res, respond);
-});
+// TODO
+//router.post('/', function(req, res, next) {
+//  search(req, res, next, respond);
+//});
 // --------------------------------------------------------------------------------------
 function search(req, res, next) {
   var dict = {};
 
+  // TODO - keys validation
+
   // debug
-  console.log("req.body:");
-  console.log(req.body);
+  console.log("req.query:");
+  console.log(req.query);
 
-  res.json("done");
 
-  if(req.body.username == "" && req.body.mac == "")   /* no main search key was entered */
-    return res.json([]);     // do not search database 
-
-  if(req.body.username != "")
-    dict["pn"] = req.body.username;
-  
-  if(req.body.mac != "")
-    dict["csi"] = req.body.mac;
-  
-  if(req.body.result != "nezadáno")
-    dict["result"] = req.body.result;
- 
-  
   // TODO
-  if(req.body.from != undefined) {
-    dict["timestamp"] = {};
-    dict["timestamp"]["$gte"] = req.body.from;
-  }
+  //if(req.query.username == "" && req.query.mac == "")   /* no main search key was entered */
+  //  return res.json([]);     // do not search database 
+
+  console.log(req.query.username);
+
+  //if(req.query.username != "")
+  //  dict["pn"] = req.query.username;
   
-  if(req.body.to != undefined) {
-    if(dict["timestamp"] == undefined)
-      dict["timestamp"] = {};
-    dict["timestamp"]["$lt"] = req.body.to;
-  }
+  if(req.query.mac_address != "")
+    dict["csi"] = req.query.mac_address;
+  
+  //if(req.query.result != "nezadáno")
+  //  dict["result"] = req.query.result;
+ 
+  //
+  //// TODO
+  //if(req.query.from != undefined) {
+  //  dict["timestamp"] = {};
+  //  dict["timestamp"]["$gte"] = req.query.from;
+  //}
+  //
+  //if(req.query.to != undefined) {
+  //  if(dict["timestamp"] == undefined)
+  //    dict["timestamp"] = {};
+  //  dict["timestamp"]["$lt"] = req.query.to;
+  //}
   
   // TODO - pridat razeni dle data od nejstarsiho po nejnovejsi ?
 
