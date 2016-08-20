022a3906cca1134a240c242f526cec7ab08aeac8
Václav Mach <vac.mach@sh.cvut.cz>
2016-08-20 15:41:43 +0000

    add routes for testing saml

---
+++ routes/saml_test.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  
+  //console.log(req);
+  console.log(req.server);
+  //console.log(req.query);   // debug
+  //console.log(req.headers);   // debug
+  //console.log(req.headers.cookie);
+  //console.log(typeof(req.headers.cookie));
+
+  //var url = require('url');
+  //var url_parts = url.parse(req.url, true);
+  //var query = url_parts.query;
+
+  //console.log(url_parts);
+  //console.log(query);
+
+
+  //res.render('index', { title: 'testovaci autentizace' });
+  res.send('autentizace selhala');
+
+  //var color = req.param('color');
+
+});
+
+module.exports = router;
