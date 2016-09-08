5c8ec86f83337ebfa97675bca4ee1ab97fec2e4f
Václav <vac.mach@sh.cvut.cz>
2016-09-08 23:17:15 +0200

    modify cron.sh

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -5,25 +5,31 @@
 # This script runs fticks_to_json.sh and is intended to be run by cron at regular intervals.
 # Script takes no arguments.
 
+# application root
+etlog_root="/home/etlog/etlog/"
+
 # current date in format YYYY-MM-DD
 date=$(date "+%Y-%m-%d")
 
 # database to which logs will be imported to
-database="live"
+database="etlog"
 
 # collection to which logs will be imported to
 collection="logs"
 
 # log file to process
-logfile="/var/log/fticks-$date"
+logfile="/var/log/fticks/fticks-$date"
 
-# error log
-errlog="/var/log/fticks_err-$date.log"
+# etlog log root
+etlog_log_root="/var/log/etlog/"
+
+# fticks to json conversion error log
+errlog="$etlog_log_root/fticks_err-$date.log"
 
 # mongo error log
-mongo_errlog="/var/log/mongo_err-$date.log"
+mongo_errlog="$etlog_log_root/mongo_err-$date.log"
 
 # convert to json and import to database
-/var/www/radlog/scripts/fticks_to_json.sh $logfile 2>>$errlog | mongoimport -d $database -c $collection 2>>$mongo_errlog
+$etlog_root/scripts/fticks_to_json.sh $logfile 2>>$errlog | mongoimport -d $database -c $collection 2>>$mongo_errlog
 
 exit 0
