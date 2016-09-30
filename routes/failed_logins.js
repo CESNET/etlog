53eb80d4663fe86e23b52274679383d9379ac6d0
Václav <vac.mach@sh.cvut.cz>
2016-10-01 01:58:19 +0200

    add basic failed logins api

--- routes/failed_logins.js
+++ routes/failed_logins.js
@@ -1,7 +1,40 @@
-var express = require('express');
-var router = express.Router();
-// -----------------------------------------------------------------
+const express = require('express');
+const router = express.Router();
+const aqp = require('api-query-params').default;    // uses ES6
+// --------------------------------------------------------------------------------------
+// get failed records for specific date
+// --------------------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  res.render('failed_logins', { title: 'vyhledávání neúspěšných pokusů o ověření' });
+  var query = aqp(req.query, {          // parse query string
+    whitelist : [ 'username', 'timestamp', 'fail_count', 'ok_count', 'ratio' ]       // whitelist collection variables
+  });
+
+  // TODO - validation !!
+  // TODO - another interval than day !!
+  // TODO - check that timestamp is present
+  // do not search otherwise
+
+  search(req, res, respond, query);
 });
+// --------------------------------------------------------------------------------------
+// search database for specified data
+// --------------------------------------------------------------------------------------
+function search(req, res, respond, query) {
+  req.db.failed_logins.find(query.filter,  { _id : 0, timestamp : 0}, function(err, items) {
+    respond(err, items, res)
+  });
+}
+// --------------------------------------------------------------------------------------
+// send data to user
+// --------------------------------------------------------------------------------------
+function respond(err, items, res) {
+  if(err) {
+    console.log(err);
+    res.send(err);
+    return;
+  }
+  
+  res.json(items);
+}
+// --------------------------------------------------------------------------------------
 module.exports = router;
