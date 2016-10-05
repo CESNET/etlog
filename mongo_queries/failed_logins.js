68206163779be0ef1c303c857a4e71ae05b4eee2
Václav <vac.mach@sh.cvut.cz>
2016-10-05 18:14:37 +0200

    fix db variable

--- mongo_queries/failed_logins.js
+++ mongo_queries/failed_logins.js
@@ -6,6 +6,6 @@
 var max = new Date();           // today
 var min = new Date(max);    
 min.setDate(min.getDate() -1);  // the day before
-var out = database.logs.aggregate([ { $match : { timestamp :{ $gte : min, $lt : max }, pn : { $ne : ""} } }, { $project : { timestamp : 1, pn : 1, result : 1} },  { $group : { _id : { pn : "$pn", result : "$result" }, count : { $sum : 1 } } }, { $group : { _id : { pn : "$_id.pn" }, results : { $addToSet : "$$ROOT._id.result" }, result_count : { $addToSet : "$count" }, } }, { $match : { results : { $in : [ "FAIL" ] } } }]) 
+var out = db.logs.aggregate([ { $match : { timestamp :{ $gte : min, $lt : max }, pn : { $ne : ""} } }, { $project : { timestamp : 1, pn : 1, result : 1} },  { $group : { _id : { pn : "$pn", result : "$result" }, count : { $sum : 1 } } }, { $group : { _id : { pn : "$_id.pn" }, results : { $addToSet : "$$ROOT._id.result" }, result_count : { $addToSet : "$count" }, } }, { $match : { results : { $in : [ "FAIL" ] } } }]) 
 printjson(out)
 
