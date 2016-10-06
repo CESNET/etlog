e5c6e29364523653064027e5b1dc57bb4316a1bc
Václav <vac.mach@sh.cvut.cz>
2016-10-06 19:33:36 +0200

    update route to use api-query-params

--- routes/roaming_most_provided.js
+++ routes/roaming_most_provided.js
@@ -1,76 +1,57 @@
 var express = require('express');
 var router = express.Router();
+const aqp = require('api-query-params').default;    // uses ES6
+// --------------------------------------------------------------------------------------
+// get roaming data for organisations most providing roaming
+// timestamp, [inst_name, provided_count]
 // --------------------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  res.render('roaming', { title: 'Ukazkove rozhrani pro vyhledavani nad radius logy' });
-});
-// --------------------------------------------------------------------------------------
-router.post('/search', function(req, res, next) {
-  search(req, res, respond);
-});
-// --------------------------------------------------------------------------------------
-function search(req, res) {
-  // TODO - timestamp
-  
-  min_date = new Date("2015-04-23T00:00:00Z");
-  max_date = new Date(min_date.getTime() + 86400000);
+  var qs = req.url.substr(2);   // remove '/?'
 
-  req.db.logs.aggregate(
-  [ 
-  { 
-    $match : 
-      { 
-        timestamp : 
-          {
-            $gte : min_date, 
-            $lt : max_date 
-          }, 
-        result : "OK"         // only successfully authenticated users
-      } 
-  },  
-  {
-    $group :                  // group by pair [realm, csi] - normalization by mac address
-    { 
-      _id : 
-        { 
-          realm : "$realm", 
-          csi : "$csi" 
-        } 
-    } 
-  },
-  { 
-    $project : 
-      { 
-        "_id.realm" : 1             // we need only realm
-      } 
-  }, 
-  { 
-    $group :                // group by realm
-      { 
-        _id : 
-          { 
-            realm : "$_id.realm" 
-          }, 
-        count : 
-          { 
-            $sum : 1        // count number of records for given realm
-          } 
-      } 
-  }, 
-  { 
-    $sort :                 // sort from lowest to highest
-      { 
-        count : 1
-      } 
-  }
-  ],
-    function(err, items) {
-      if(err == null)
-        items = filter(items);
-
-      respond(err, items, res);
+  var query = aqp(qs, {         // parse query string
+    whitelist : [ 'timestamp', 'inst_name', 'provided_count' ]       // whitelist collection keys
   });
-}
+
+  if(query.filter.timestamp == undefined) {    // do not search if timestamp is not defined
+    res.end("timestamp must be defined!");
+    return;
+  }
+
+  // validation
+  var keys = query.filter.timestamp;
+  var range = [];
+
+  for(var key in keys) {
+    range.push(keys[key]);      // for further processing
+
+    if(isNaN(Date.parse(keys[key]))) {    // invalid date
+      res.end("invalid date: " + keys[key]);
+      return;
+    }
+  }
+  
+  // valid cases for timestamp
+  //
+  // = exactly one day
+  // = range from, to
+  //
+  // TODO - distinct date at 00:00:00:000 and other dates
+  // needs to be implemented?
+  if(typeof(query.filter.timestamp) == "object" && Object.keys(query.filter.timestamp).length > 1) {  // range
+    if(Math.abs(range[0] - range[1]) % 86400000 == 0) {     // exact range of days
+      search_days(req, res, respond, query);
+    }
+    else {  // another range
+      //search_interval(req, res, respond, query);
+      // TODO
+    }
+  }
+  else {    // one day only
+    search_days(req, res, respond, query);
+  }
+});
+// --------------------------------------------------------------------------------------
+// send data to user
 // --------------------------------------------------------------------------------------
 function respond(err, items, res) {
   if(err) {
@@ -82,16 +63,15 @@ function respond(err, items, res) {
   res.json(items);
 }
 // --------------------------------------------------------------------------------------
-function filter(items)
-{
-  dict = {};
+// search database for specified data
+// timestamp matches specific day or range of days
+// --------------------------------------------------------------------------------------
+function search_days(req, res, respond, query) {
+  query.filter.provided_count = query.provided_count || { "$exists" : true };   // make sure provided count is set
 
-  for(var item in items) {
-    var key = items[item];
-    dict[key._id.realm] = key.count    // save count to realm
+  req.db.roaming.find(query.filter, { _id : 0, timestamp : 0}, function(err, items) {
+    respond(err, items, res);
   }
-
-  return dict;
 }
 // --------------------------------------------------------------------------------------
 module.exports = router;
