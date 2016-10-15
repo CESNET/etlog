5dc704537b5d8ccde27fd069a2fde0d0540e81d7
Václav <vac.mach@sh.cvut.cz>
2016-10-15 10:25:47 +0200

    clean code

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,8 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
+const express = require('express');
+const router = express.Router();
+// --------------------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  
-  //console.log(req);
-  console.log(req.server);
-  //console.log(req.query);   // debug
-  //console.log(req.headers);   // debug
-  //console.log(req.headers.cookie);
-  //console.log(typeof(req.headers.cookie));
-
-  //var url = require('url');
-  //var url_parts = url.parse(req.url, true);
-  //var query = url_parts.query;
-
-  //console.log(url_parts);
-  //console.log(query);
-
-
-  res.render('index', { title: 'Ukazkove rozhrani pro vyhledavani nad radius logy' });
-
-  //var color = req.param('color');
-
+  res.render('index', { title: 'etlog' });
 });
-
+// --------------------------------------------------------------------------------------
 module.exports = router;
