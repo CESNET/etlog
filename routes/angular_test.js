6973d9bd69725c48b4ad8f0163c228bc56707327
Václav <vac.mach@sh.cvut.cz>
2016-10-19 16:04:39 +0200

    delete angular test page

--- routes/angular_test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('test', { title: 'Ukazkove rozhrani pro vyhledavani nad radius logy' });
-});
-
-module.exports = router;
