9b0a52f5535fd29ea41aed78cb68a407fbde6cf6
Václav <vac.mach@sh.cvut.cz>
2016-10-20 17:14:42 +0200

    update query parser api

--- routes/query_parser.js
+++ routes/query_parser.js
@@ -6,24 +6,23 @@ exp = {};
 // args:
 // 1) request url
 // 2) array of collection keys, that should be whitelisted
-// 3) function for interval of days - days have to start at 00:00:00
-// 4) function for other interval 
+// 3) function to validate time interval
 // --------------------------------------------------------------------------------------
-exp.parse_query_string = function(url, whitelist, search_days, search_interval) {
+exp.parse_query_string = function(url, whitelist, validate) {
 
-  var qs = url.substr(2);   // remove '/?'
+  var qs = url.match(/\?.*$/)[0];     // extract query string part from request url
+  qs = qs.substr(1);   // remove '?'
 
   var query = aqp(qs, {         // parse query string
     whitelist : whitelist       // whitelist collection keys
   });
 
-  // TODO - err status
   if(query.filter.timestamp == undefined) {    // do not search if timestamp is not defined
-    res.status(500).send({ error : "timestamp must be defined!"});        // send error status and message
-    return;
+    throw { error : "timestamp must be defined!"};
   }
 
   // validation
+  // TODO
   var keys = query.filter.timestamp;
   var range = [];
 
@@ -36,30 +35,44 @@ exp.parse_query_string = function(url, whitelist, search_days, search_interval) 
     }
   }
   
-  // debug
-  console.log(query);
-  
-  // valid cases for timestamp
-  //
-  // = exactly one day
-  // = range from, to
-  //
-  // TODO - distinct date at 00:00:00:000 and other dates
-  // needs to be implemented?
-  
-  // TODO - day start at .. 
+  if(!validate(query)) {  // check correctly specified time interval
+    throw { error : "incorrect interval specification!"};
+  }
 
+  return query;
+}
+// --------------------------------------------------------------------------------------
+// validate query for correct time interval
+// --------------------------------------------------------------------------------------
+exp.validate_days = function(query) {
   if(typeof(query.filter.timestamp) == "object" && Object.keys(query.filter.timestamp).length > 1) {  // range
-    if(Math.abs(range[0] - range[1]) % 86400000 == 0) {     // exact range of days
-      return { query: query, search : search_days };
-    }
-    else {  // another range
-      return { query: query, search : search_interval };
+    var keys = Object.keys(query.filter.timestamp);
+    for(var key in keys) {
+      if(!(query.filter.timestamp[keys[key]].getHours() == 0 && query.filter.timestamp[keys[key]].getMinutes() == 0 
+        && query.filter.timestamp[keys[key]].getSeconds() == 0 && query.filter.timestamp[keys[key]].getMilliseconds() == 0))
+        return false;       // some part is set incorrectly
     }
   }
   else {    // one day only
-    return { query: query, search : search_days };
+    return (query.filter.timestamp.getHours() == 0 && query.filter.timestamp.getMinutes() == 0 
+    && query.filter.timestamp.getSeconds() == 0 && query.filter.timestamp.getMilliseconds() == 0);
   }
+
+  return true;
+}
+// --------------------------------------------------------------------------------------
+// validate query for correct time interval
+// --------------------------------------------------------------------------------------
+exp.validate_interval = function(query) {
+  if(typeof(query.filter.timestamp) == "object" && Object.keys(query.filter.timestamp).length > 1) {  // range
+    // is any checking needed here ?
+    // TODO
+  }
+  else {    // one day only is not allowed !
+    return false;
+  }
+
+  return true;
 }
 // --------------------------------------------------------------------------------------
 module.exports = exp;
