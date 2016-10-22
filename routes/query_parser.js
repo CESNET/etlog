e00f2774f196e23a294b67f766fdbcf8f3b0d81a
Václav <vac.mach@sh.cvut.cz>
2016-10-22 14:06:17 +0200

    fix query string matching

--- routes/query_parser.js
+++ routes/query_parser.js
@@ -12,8 +12,12 @@ exp = {};
 //    defines if both dates (starting and ending are requied)
 // --------------------------------------------------------------------------------------
 exp.parse_query_string = function(url, whitelist, validate, date_req) {
+  var qs = url.match(/\?.*$/);  // try to match query string if present
 
-  var qs = url.match(/\?.*$/)[0];     // extract query string part from request url
+  if(!qs)
+    throw { error : "no query string present!"};
+
+  qs = qs[0];           // extract query string part from request url
   qs = qs.substr(1);   // remove '?'
 
   var query = aqp(qs, {         // parse query string
