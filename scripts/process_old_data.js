c45bca18c4edd4a1dd86ee957ee82cdbd241c2e0
Václav <vac.mach@sh.cvut.cz>
2016-10-23 21:43:03 +0200

    add database

--- scripts/process_old_data.js
+++ scripts/process_old_data.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 // --------------------------------------------------------------------------------------
+const database = require( '../db' );
 const failed_logins = require('../cron/failed_logins.js');
 const invalid_records = require('../cron/invalid_records.js');
 const mac_count = require('../cron/mac_count.js');
