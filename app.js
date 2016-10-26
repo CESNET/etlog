0cc8a209be0d5d9493d7b5827c0cce9aa64ce142
Václav <vac.mach@sh.cvut.cz>
2016-10-26 16:32:42 +0200

    add favicon

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('view engine', 'pug');
 
 // --------------------------------------------------------------------------------------
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
