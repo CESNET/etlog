eed23392fb4ca9a8136739064236086035457f27
Václav <vac.mach@sh.cvut.cz>
2016-10-26 07:31:03 +0200

    remove invalid records task

--- scripts/process_old_data.js
+++ scripts/process_old_data.js
@@ -4,7 +4,6 @@
 const database = require( '../db' );
 const async = require( 'async' );
 const failed_logins = require('../cron/failed_logins.js');
-const invalid_records = require('../cron/invalid_records.js');
 const mac_count = require('../cron/mac_count.js');
 const roaming = require('../cron/roaming.js');
 const users_mac = require('../cron/users_mac.js');
@@ -18,9 +17,6 @@ async.series([
   },
   function(callback) {
     failed_logins.process_old_data(database, callback);
-  },
-  function(callback) {
-    invalid_records.process_old_data(database, callback);
   },
   function(callback) {
     mac_count.process_old_data(database, callback);
