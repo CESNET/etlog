17ccd67081205ed9bc5212c4a0fa0c06b05d0c95
Václav <vac.mach@sh.cvut.cz>
2016-10-28 21:04:33 +0200

    use const for required files

--- routes/roaming_most_used.js
+++ routes/roaming_most_used.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const qp = require('./query_parser');
 // --------------------------------------------------------------------------------------
 // get roaming data for organisations most using roaming
--- routes/shared_mac.js
+++ routes/shared_mac.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const qp = require('./query_parser');
 // --------------------------------------------------------------------------------------
 // get shared mac data
