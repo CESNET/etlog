48e27465f07c6fc8d8529a3a9710690b9b4b8fd4
Václav <vac.mach@sh.cvut.cz>
2016-11-02 16:30:47 +0100

    add functionality for days

--- request.js
+++ request.js
@@ -64,4 +64,40 @@ function failed_to_human_readable(data)
   return ret;
 }
 // --------------------------------------------------------------------------------------
+// returns failed logins for given date and realm
+// --------------------------------------------------------------------------------------
+exp.get_failed_logins_daily = function(date, realm, callback)
+{
+  var url = "/failed_logins/";
+
+  var max = new Date(date);
+  max.setHours(0);
+  max.setMinutes(0);
+  max.setSeconds(0);
+  max.setMilliseconds(0);  // set to 00:00:00:000
+
+  var min = new Date(max);
+  min.setDate(max.getDate() - 1);  // 1 day before
+
+  var query = '?timestamp>=' + min.toISOString() + "&timestamp<" + max.toISOString();  // use ISO-8601 format
+  query += "&username=/.*@" + realm + "$/";          // limit by domain part uf username => realm
+
+  request.get({
+    url: url_base + url + query     // use query string here for simple usage
+  }, function (error, response, body) {
+    if(error)
+      console.log(error);
+    else {
+      callback(null, JSON.parse(body));   // return response to caller
+    }
+  });
+}
+// --------------------------------------------------------------------------------------
+// returns successful logins for given date and realm
+// --------------------------------------------------------------------------------------
+exp.get_succ_logins_daily = function(date, realm, callback)
+{
+  // TODO
+}
+// --------------------------------------------------------------------------------------
 module.exports = exp;
