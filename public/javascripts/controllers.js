f8c1248e3ba41a6a5bf6cd3e2f2cab47fa8eb8bc
Václav <vac.mach@sh.cvut.cz>
2016-11-18 22:00:18 +0100

    remove unsused code

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1024,19 +1024,6 @@ function addiational_fields_mac_count($scope)
   };
 }
 // --------------------------------------------------------------------------------------
-// TODO
-// --------------------------------------------------------------------------------------
-function table($scope)
-{
-  //console.log($scope.data);
-
-  //$scope.rowCollection = [
-  //  {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-  //  {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-  //  {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
-  //];
-}
-// --------------------------------------------------------------------------------------
 // draw graph with data from api
 // --------------------------------------------------------------------------------------
 function graph($scope)
