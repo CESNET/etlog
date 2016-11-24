f91055ad0c8440f8878609a1fc9fda4d35bb27a8
Václav <vac.mach@sh.cvut.cz>
2016-11-25 00:04:12 +0100

    add succ logins route

---
+++ routes/succ_logins.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const router = express.Router();
+const qp = require('./query_parser');
+const agg = require('./aggregation');
+// --------------------------------------------------------------------------------------
+// get successful logins data for day(s)
+// --------------------------------------------------------------------------------------
+router.get('/', function(req, res, next) {
+  try {
+    var query = qp.parse_query_string(req.url,
+      ['username', 'timestamp', 'count'],
+      qp.validate_days);
+  }
+  catch(error) {
+    var err = new Error(error.error);
+    err.status = 400;
+    next(err);
+    return;
+  }
+
+  search_days(req, res, next, query);     // perform search with constructed mongo query
+});
+// --------------------------------------------------------------------------------------
+// search database for specified data
+// timestamp matches specific day or range of days
+// --------------------------------------------------------------------------------------
+function search_days(req, res, next, query) {
+  // query for username with records for multiple days
+  // gets results separated for each day !
+  // this is defined by the query -> needs to be computed on the fly
+
+  // ===================================================
+  // if query.filter contains some conditions for data eg. count,
+  // the condition must be applied to result of all records aggregated across given timestamps
+  // -> the condition must be applied after aggregation !
+
+  var cond = agg.check_filter(query.filter, [ "count" ]);
+
+  // ===================================================
+  // construct base query
+  var aggregate_query = [
+    {
+      $match : query.filter       // filter by query
+    },
+    {
+      $group :
+        {
+          _id :
+            {
+              username : "$username"  // group by username -> query on multiple records, we want only on record on the output
+            },
+          count :
+            {
+              $sum : "$count"      // sum count
+            },
+        }
+    },
+    {
+      $project :
+        {
+          count : 1,
+          username : "$_id.username",   // id.username -> username
+          _id : 0
+        }
+    }
+  ];
+
+  // ===================================================
+  // add condition from original filter if defined
+  agg.add_cond(aggregate_query, cond);
+
+  // ===================================================
+  // add other operators, if defined in query
+  agg.add_ops(aggregate_query, query);
+
+  // ===================================================
+  // search
+
+  req.db.succ_logins.aggregate(aggregate_query,
+  function(err1, items) {
+    if(err1) {
+      var err2 = new Error();      // just to detect where the original error happened
+      console.error(err1);
+      console.error(err2);
+      next([err2, err1]);
+      return;
+    }
+
+    respond(items, res);
+  });
+}
+// --------------------------------------------------------------------------------------
+// send data to user
+// --------------------------------------------------------------------------------------
+function respond(items, res) {
+  res.json(items);
+}
+// --------------------------------------------------------------------------------------
+module.exports = router;
