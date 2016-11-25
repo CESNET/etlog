c0df5f986b3e9c6bc7c3d095312d913c46ad40db
Václav <vac.mach@sh.cvut.cz>
2016-11-26 00:04:32 +0100

    dynamically change title

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,3 +2,11 @@
 // define angular module/app
 var etlog = angular.module('etlog', ['ui.router', 'angularUtils.directives.dirPagination' ]);
 // --------------------------------------------------------------------------------------
+// set page title
+// --------------------------------------------------------------------------------------
+etlog.run(['$rootScope', function($rootScope) {
+  $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    $rootScope.title = toState.title;   // set current state title
+  });
+}]);
+// --------------------------------------------------------------------------------------
--- public/javascripts/routes.js
+++ public/javascripts/routes.js
@@ -5,46 +5,79 @@ angular.module('etlog').config(function($stateProvider, $urlRouterProvider) {
   .state('search', {
     url: '/search?pn&csi',
     templateUrl: '/partials/search.html',
+    title : 'etlog: obecné vyhledávání'
   })
 
   .state('mac_count', {
     url: '/mac_count',
-    templateUrl: '/partials/mac_count.html'
+    templateUrl: '/partials/mac_count.html',
+    title : 'etlog: počet zařízení'
   })
 
   .state('failed_logins', {
     url: '/failed_logins',
-    templateUrl: '/partials/failed_logins.html'
+    templateUrl: '/partials/failed_logins.html',
+    title : 'etlog: neúspěšná přihlášení'
   })
 
   .state('shared_mac', {
     url: '/shared_mac',
-    templateUrl: '/partials/shared_mac.html'
+    templateUrl: '/partials/shared_mac.html',
+    title : 'etlog: sdílená zařízení'
+  })
+
+  .state('graph_test_2', {
+    url: '/graph_test_2',
+    templateUrl: '/partials/graph_test_2.html',
+    title : 'etlog: graph test 2'
+  })
+
+  .state('graph_test_3', {
+    url: '/graph_test_3',
+    templateUrl: '/partials/graph_test_3.html',
+    title : 'etlog: graph test 3'
   })
 
   .state('roaming_most_provided', {
     url: '/roaming_most_provided',
-    templateUrl: '/partials/roaming_most_provided.html'
+    templateUrl: '/partials/roaming_most_provided.html',
+    title : 'etlog: nejvíce poskytovaný roaming'
   })
 
   .state('roaming_most_used', {
     url: '/roaming_most_used',
-    templateUrl: '/partials/roaming_most_used.html'
+    templateUrl: '/partials/roaming_most_used.html',
+    title : 'etlog: nejvíce využívaný roaming'
   })
 
   .state('roaming_most_used_test', {
     url: '/roaming_most_used_test',
-    templateUrl: '/partials/roaming_most_used_test.html'
+    templateUrl: '/partials/roaming_most_used_test.html',
+    title : 'etlog: roaming test 1'
   })
 
   .state('roaming_most_provided_test', {
     url: '/roaming_most_provided_test',
-    templateUrl: '/partials/roaming_most_provided_test.html'
+    templateUrl: '/partials/roaming_most_provided_test.html',
+    title : 'etlog: roaming test 2'
   })
 
   .state('roaming_activity', {
     url: '/roaming_activity',
-    templateUrl: '/partials/roaming_activity.html'
+    templateUrl: '/partials/roaming_activity.html',
+    title : 'etlog: aktivita eduroamu'
+  })
+
+  .state('test', {
+    url: '/test',
+    templateUrl: '/partials/test.html',
+    title : 'etlog: test datepickeru'
+  })
+
+  .state('detection_data', {
+    url: '/detection_data',
+    templateUrl: '/partials/detection_data.html',
+    title : 'etlog: data k detekci'
   })
 });
 // --------------------------------------------------------------------------------------
