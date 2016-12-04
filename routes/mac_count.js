42e8e47ccd06dfa68a4e1187b198e08bdef4acb6
Václav <vac.mach@sh.cvut.cz>
2016-12-04 12:10:25 +0100

    use stream api

--- routes/mac_count.js
+++ routes/mac_count.js
@@ -100,8 +100,10 @@ function search_days(req, res, next, query) {
   // ===================================================
   // search
 
-  req.db.mac_count.aggregate(aggregate_query,
-  function(err1, items) {
+ var stream = req.db.mac_count.aggregate(aggregate_query).cursor({ batchSize: 1000 }).exec().stream();
+ var data = [];
+
+  stream.on('error', function (err1) {
     if(err1) {
       var err2 = new Error();      // just to detect where the original error happened
       console.error(err1);
@@ -109,8 +111,14 @@ function search_days(req, res, next, query) {
       next([err2, err1]);
       return;
     }
+  });
 
-    respond(items, res);
+  stream.on('data', function(item) {
+    data.push(item);
+  });
+
+  stream.on('end', function(items) {
+    respond(data, res);
   });
 }
 // --------------------------------------------------------------------------------------
