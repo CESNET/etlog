b73c1590e5da6d2e2ba198d007baea7753b343bd
Václav <vac.mach@sh.cvut.cz>
2016-12-11 12:20:50 +0100

    clean heat map graph

--- javascripts/angular/controllers.js
+++ javascripts/angular/controllers.js
@@ -1937,15 +1937,15 @@ function graph_heat_map($scope)
           .append("rect")
           .attr("x", function(d) { return (hccol.indexOf(d.col) + 1) * cellSize + 4; })     // compensate for labels
           .attr("y", function(d) { return (hcrow.indexOf(d.row) + 1) * cellSize + 4; })     // compensate for labels
-          .attr("class", function(d){ return "cell cell-border cr" + (d.row - 1) + " cc" + (d.col - 1); })
+          .attr("class", function(d) { return "cell cell-border cr" + (d.row - 1) + " cc" + (d.col - 1); })
           .attr("width", cellSize)
           .attr("height", cellSize)
           .style("fill", function(d) { return colorScale(d.value); })
           .on("mouseover", function(d) {
                  //highlight text
                  d3.select(this).classed("cell-hover", true);
-                 d3.selectAll(".rowLabel").classed("text-highlight", function(r, ri){ return ri == d.row; });
-                 d3.selectAll(".colLabel").classed("text-highlight", function(c, ci){ return ci == d.col; });
+                 d3.selectAll(".rowLabel").classed("text-highlight", function(r, ri) { return ri == d.row; });
+                 d3.selectAll(".colLabel").classed("text-highlight", function(c, ci) { return ci == d.col; });
                  //Update the tooltip position and value
                  d3.select("#tooltip")
                    .style("left", (d3.event.pageX + 10) + "px")
@@ -1956,37 +1956,14 @@ function graph_heat_map($scope)
                  d3.select("#tooltip").classed("hidden", false);
           })
           .on("mouseout", function(){
-                 d3.select(this).classed("cell-hover",false);
-                 d3.selectAll(".rowLabel").classed("text-highlight",false);
-                 d3.selectAll(".colLabel").classed("text-highlight",false);
+                 d3.select(this).classed("cell-hover", false);
+                 d3.selectAll(".rowLabel").classed("text-highlight", false);
+                 d3.selectAll(".colLabel").classed("text-highlight", false);
                  d3.select("#tooltip").classed("hidden", true);
           })
           ;
 
   // ==========================================================
-
-    //var legend = svg.selectAll(".legend")
-    //    .data([-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10])
-    //    .enter().append("g")
-    //    .attr("class", "legend");
-   
-    //legend.append("rect")
-    //  .attr("x", function(d, i) { return legendElementWidth * i; })
-    //  .attr("y", height + (cellSize * 2))
-    //  .attr("width", legendElementWidth)
-    //  .attr("height", cellSize)
-    //  .style("fill", function(d, i) { return colors[i]; });
-    //  //.style("fill", function(d, i) { return colorScale(i); });
-   
-    //legend.append("text")
-    //  .attr("class", "mono")
-    //  .text(function(d) { return d; })
-    //  .attr("width", legendElementWidth)
-    //  .attr("x", function(d, i) { return legendElementWidth * i; })
-    //  .attr("y", height + (cellSize*4));
-
-  // ==========================================================
-
   // Change ordering of cells
 
     function sortbylabel(rORc, i, sortOrder){
@@ -2020,161 +1997,61 @@ function graph_heat_map($scope)
 
   // ==========================================================
 
-    d3.select("#order").on("change",function(){
-      order(this.value);
-    });
-    
-    function order(value){
-     if(value == "hclust"){
-      var t = svg.transition().duration(3000);
-      t.selectAll(".cell")
-        .attr("x", function(d) { return hccol.indexOf(d.col) * cellSize; })
-        .attr("y", function(d) { return hcrow.indexOf(d.row) * cellSize; })
-        ;
-
-      t.selectAll(".rowLabel")
-        .attr("y", function (d, i) { return hcrow.indexOf(i+1) * cellSize; })
-        ;
-
-      t.selectAll(".colLabel")
-        .attr("y", function (d, i) { return hccol.indexOf(i+1) * cellSize; })
-        ;
-
-     }else if (value=="probecontrast"){
-      var t = svg.transition().duration(3000);
-      t.selectAll(".cell")
-        .attr("x", function(d) { return (d.col - 1) * cellSize; })
-        .attr("y", function(d) { return (d.row - 1) * cellSize; })
-        ;
-
-      t.selectAll(".rowLabel")
-        .attr("y", function (d, i) { return i * cellSize; })
-        ;
-
-      t.selectAll(".colLabel")
-        .attr("y", function (d, i) { return i * cellSize; })
-        ;
-
-     }else if (value=="probe"){
-      var t = svg.transition().duration(3000);
-      t.selectAll(".cell")
-        .attr("y", function(d) { return (d.row - 1) * cellSize; })
-        ;
-
-      t.selectAll(".rowLabel")
-        .attr("y", function (d, i) { return i * cellSize; })
-        ;
-     }else if (value=="contrast"){
-      var t = svg.transition().duration(3000);
-      t.selectAll(".cell")
-        .attr("x", function(d) { return (d.col - 1) * cellSize; })
-        ;
-      t.selectAll(".colLabel")
-        .attr("y", function (d, i) { return i * cellSize; })
-        ;
-     }
-    }
-  
-    //// ==========================================================
-
-    //var sa = d3.select(".g3")
-    //    .on("mousedown", function() {
-    //        if( !d3.event.altKey) {
-    //           d3.selectAll(".cell-selected").classed("cell-selected",false);
-    //           d3.selectAll(".rowLabel").classed("text-selected",false);
-    //           d3.selectAll(".colLabel").classed("text-selected",false);
-    //        }
-    //       var p = d3.mouse(this);
-    //       sa.append("rect")
-    //       .attr({
-    //           rx      : 0,
-    //           ry      : 0,
-    //           class   : "selection",
-    //           x       : p[0],
-    //           y       : p[1],
-    //           width   : 1,
-    //           height  : 1
-    //       })
-    //    })
-    //    .on("mousemove", function() {
-    //       var s = sa.select("rect.selection");
-    //    
-    //       if(!s.empty()) {
-    //           var p = d3.mouse(this),
-    //               d = {
-    //                   x       : parseInt(s.attr("x"), 10),
-    //                   y       : parseInt(s.attr("y"), 10),
-    //                   width   : parseInt(s.attr("width"), 10),
-    //                   height  : parseInt(s.attr("height"), 10)
-    //               },
-    //               move = {
-    //                   x : p[0] - d.x,
-    //                   y : p[1] - d.y
-    //               }
-    //           ;
-    //    
-    //           if(move.x < 1 || (move.x*2<d.width)) {
-    //               d.x = p[0];
-    //               d.width -= move.x;
-    //           } else {
-    //               d.width = move.x;       
-    //           }
-    //    
-    //           if(move.y < 1 || (move.y*2<d.height)) {
-    //               d.y = p[1];
-    //               d.height -= move.y;
-    //           } else {
-    //               d.height = move.y;       
-    //           }
-    //           s.attr(d);
-    //    
-    //               // deselect all temporary selected state objects
-    //           d3.selectAll('.cell-selection.cell-selected').classed("cell-selected", false);
-    //           d3.selectAll(".text-selection.text-selected").classed("text-selected",false);
-
-    //           d3.selectAll('.cell').filter(function(cell_d, i) {
-    //               if(
-    //                   !d3.select(this).classed("cell-selected") && 
-    //                       // inner circle inside selection frame
-    //                   (this.x.baseVal.value)+cellSize >= d.x && (this.x.baseVal.value)<=d.x+d.width && 
-    //                   (this.y.baseVal.value)+cellSize >= d.y && (this.y.baseVal.value)<=d.y+d.height
-    //               ) {
-    //    
-    //                   d3.select(this)
-    //                   .classed("cell-selection", true)
-    //                   .classed("cell-selected", true);
-
-    //                   d3.select(".r"+(cell_d.row-1))
-    //                   .classed("text-selection",true)
-    //                   .classed("text-selected",true);
-
-    //                   d3.select(".c"+(cell_d.col-1))
-    //                   .classed("text-selection",true)
-    //                   .classed("text-selected",true);
-    //               }
-    //           });
-    //       }
-    //    })
-    //    .on("mouseup", function() {
-    //          // remove selection frame
-    //       sa.selectAll("rect.selection").remove();
-    //    
-    //           // remove temporary selection marker class
-    //       d3.selectAll('.cell-selection').classed("cell-selection", false);
-    //       d3.selectAll(".text-selection").classed("text-selection",false);
-    //    })
-    //    .on("mouseout", function() {
-    //       if(d3.event.relatedTarget.tagName=='html') {
-    //               // remove selection frame
-    //           sa.selectAll("rect.selection").remove();
-    //               // remove temporary selection marker class
-    //           d3.selectAll('.cell-selection').classed("cell-selection", false);
-    //           d3.selectAll(".rowLabel").classed("text-selected",false);
-    //           d3.selectAll(".colLabel").classed("text-selected",false);
-    //       }
-    //    })
+    //d3.select("#order").on("change",function(){
+    //  order(this.value);
+    //});
+    //
+    //function order(value){
+    // if(value == "hclust"){
+    //  var t = svg.transition().duration(3000);
+    //  t.selectAll(".cell")
+    //    .attr("x", function(d) { return hccol.indexOf(d.col) * cellSize; })
+    //    .attr("y", function(d) { return hcrow.indexOf(d.row) * cellSize; })
     //    ;
-    //// ==========================================================
+
+    //  t.selectAll(".rowLabel")
+    //    .attr("y", function (d, i) { return hcrow.indexOf(i+1) * cellSize; })
+    //    ;
+
+    //  t.selectAll(".colLabel")
+    //    .attr("y", function (d, i) { return hccol.indexOf(i+1) * cellSize; })
+    //    ;
+
+    // }else if (value=="probecontrast"){
+    //  var t = svg.transition().duration(3000);
+    //  t.selectAll(".cell")
+    //    .attr("x", function(d) { return (d.col - 1) * cellSize; })
+    //    .attr("y", function(d) { return (d.row - 1) * cellSize; })
+    //    ;
+
+    //  t.selectAll(".rowLabel")
+    //    .attr("y", function (d, i) { return i * cellSize; })
+    //    ;
+
+    //  t.selectAll(".colLabel")
+    //    .attr("y", function (d, i) { return i * cellSize; })
+    //    ;
+
+    // }else if (value=="probe"){
+    //  var t = svg.transition().duration(3000);
+    //  t.selectAll(".cell")
+    //    .attr("y", function(d) { return (d.row - 1) * cellSize; })
+    //    ;
+
+    //  t.selectAll(".rowLabel")
+    //    .attr("y", function (d, i) { return i * cellSize; })
+    //    ;
+    // }else if (value=="contrast"){
+    //  var t = svg.transition().duration(3000);
+    //  t.selectAll(".cell")
+    //    .attr("x", function(d) { return (d.col - 1) * cellSize; })
+    //    ;
+    //  t.selectAll(".colLabel")
+    //    .attr("y", function (d, i) { return i * cellSize; })
+    //    ;
+    // }
+    //}
+    // ==========================================================
 }
 // --------------------------------------------------------------------------------------
 function stacked_graph($scope)
