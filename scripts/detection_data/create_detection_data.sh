4a6ef20a89e9df8ca6a6da7073cfdc575c76eb37
Václav <vac.mach@sh.cvut.cz>
2017-01-04 14:04:03 +0100

    fix detection data generation

--- scripts/detection_data/create_detection_data.sh
+++ scripts/detection_data/create_detection_data.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cp scripts/detection_data/base.html public/partials/detection_data.html;
-node scripts/detection_data/detection_data.js >> public/partials/detection_data.html
+etlog_base="/home/etlog/etlog/"
+
+cp $etlog_base/scripts/detection_data/base.html $etlog_base/public/partials/detection_data.html;
+node $etlog_base/scripts/detection_data/detection_data.js >> $etlog_base/public/partials/detection_data.html
 
 echo "detection data generation finished"
 echo "data are available at https://etlog.cesnet.cz/#/detection_data"
 
-cp scripts/detection_data/base.html public/partials/detection_data_grouped.html;
-node scripts/detection_data/detection_data.js "grouped" >> public/partials/detection_data_grouped.html
+cp $etlog_base/scripts/detection_data/base.html $etlog_base/public/partials/detection_data_grouped.html;
+node $etlog_base/scripts/detection_data/detection_data.js "grouped" >> $etlog_base/public/partials/detection_data_grouped.html
 
 echo "grouped detection data generation finished"
 echo "data are available at https://etlog.cesnet.cz/#/detection_data_grouped"
