ec9383433263389fc89657dc1ddc42297dc66b0e
Václav <vac.mach@sh.cvut.cz>
2017-01-05 19:40:19 +0100

    add concurrent users task

--- scripts/process_old_data.js
+++ scripts/process_old_data.js
@@ -12,6 +12,7 @@ const heat_map = require('../cron/heat_map.js');
 const realm_logins = require('../cron/realm_logins.js');
 const visinst_logins = require('../cron/visinst_logins.js');
 const unique_users = require('../cron/unique_users.js');
+const concurrent_users = require('../cron/concurrent_users.js');
 // --------------------------------------------------------------------------------------
 // launch all tasks in defined order
 // every task is launched after previous is finished
@@ -46,6 +47,9 @@ async.series([
   },
   function(callback) {
     unique_users.process_old_data(database, callback);
+  },
+  function(callback) {
+    concurrent_users.process_old_data(database, callback);
   },
   ],
   function(err, results) {
