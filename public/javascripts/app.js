cda709d48735ac0017d5df2ca190a7772588c3c8
Václav <vac.mach@sh.cvut.cz>
2017-01-17 12:28:24 +0100

    new menu structure

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5809,6 +5809,26 @@ var e=c.find(".active:last a"),f=a.Event("hide.bs.tab",{relatedTarget:b[0]}),g=a
   }
 })();
 
+/*
+taken from http://bootsnipp.com/snippets/featured/multi-level-navbar-menu
+*/
+
+$(document).ready(function() {
+    $('.navbar a.dropdown-toggle').on('click', function(e) {
+        var $el = $(this);
+        var $parent = $(this).offsetParent(".dropdown-menu");
+        $(this).parent("li").toggleClass('open');
+
+        if(!$parent.parent().hasClass('nav')) {
+            $el.next().css({"top": $el[0].offsetTop, "left": $parent.outerWidth() - 4});
+        }
+
+        $('.nav li.open').not($(this).parents("li")).removeClass("open");
+
+        return false;
+    });
+});
+
 /*! Respond.js v1.4.2: min/max-width media query polyfill * Copyright 2013 Scott Jehl
  * Licensed under https://github.com/scottjehl/Respond/blob/master/LICENSE-MIT
  *  */
