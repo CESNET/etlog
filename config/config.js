fe2b667ed9d6dc66cb59976649a5250ffd17ccff
lager <machv@cesnet.cz>
2017-03-14 17:33:23 +0100

    define network listening in config

--- config/config.js
+++ config/config.js
@@ -6,6 +6,10 @@ module.exports = {
   // number of failed logins records which will be send to the administrators
   failed_logins_lines : 100,
   // subject of failed logins report
-  failed_logins_subj : "měsíční report - neúspěšná přihlášení"
+  failed_logins_subj : "měsíční report - neúspěšná přihlášení",
+  // ipv4 listening address
+  ipv4 : '78.128.211.129',
+  // ipv6 listening address
+  ipv6 : '2001:718:1:1f:50:56ff:feee:129',
 }
 // ==========================================================================================
