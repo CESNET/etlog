8514f565c0a030b0cc3499dd275695625551551f
lager <machv@cesnet.cz>
2017-04-13 14:32:24 +0200

    add info about logged in user

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 // --------------------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'etlog' });
+  res.render('index', { title: 'etlog', username : req.header("REMOTE_USER") });
 });
 // --------------------------------------------------------------------------------------
 module.exports = router;
