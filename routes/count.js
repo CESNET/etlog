11fafa836eae717e79a7e4b2aeed6062b942386f
lager <machv@cesnet.cz>
2017-04-18 22:44:16 +0200

    implement two stage regex matching in count api

--- routes/count.js
+++ routes/count.js
@@ -172,18 +172,45 @@ router.get('/logs', function(req, res, next) {
     return;
   }
 
-  req.db.logs.count(query.filter,
-  function(err1, items) {
-    if(err1) {
-      var err2 = new Error();      // just to detect where the original error happened
-      console.error(err1);
-      console.error(err2);
-      next([err2, err1]);
-      return;
-    };
+  // exclude possible regex from
+  var regex = agg.check_regex(query.filter);
 
-    respond(items, res)
-  });
+  // ===================================================
+  // construct base query
+  var aggregate_query = [
+    {
+      $match : query.filter       // filter by query
+    },
+  ];
+
+  if(Object.keys(regex).length > 0) {
+    agg.add_stage(aggregate_query, { $match : regex });
+  }
+
+  agg.add_stage(aggregate_query, {
+      $project :
+        {
+          timestamp : 1,
+          realm : 1,
+          viscountry : 1,
+          visinst : 1,
+          mac_address : "$csi",
+          username : "$pn",
+          result : 1,
+          _id : 0
+        }
+    });
+
+  // ===================================================
+  // add other operators, if defined in query
+  agg.add_ops(aggregate_query, query);
+
+  // ===================================================
+
+  aggregate_query.push({ $group: { _id: null, count: { $sum: 1 } } });       // group just to count number of records
+  aggregate_query.push({ $project : { count : 1, _id : 0 } });
+
+  get_record_count(req.db.logs, res, next, aggregate_query, transform);       // perform search with constructed mongo query
 });
 // --------------------------------------------------------------------------------------
 // get count for concurrent users
