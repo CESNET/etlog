4ba8c20ab35bc4c1eb56193f8ab72e0ad0e40345
lager <machv@cesnet.cz>
2017-04-19 21:30:22 +0200

    rewrite error reporting

--- routes/query_parser.js
+++ routes/query_parser.js
@@ -16,8 +16,11 @@ exp = {};
 exp.parse_query_string = function(url, whitelist, validate, date_req) {
   var qs = url.match(/\?.*$/);  // try to match query string if present
 
-  if(!qs)
-    throw { error : "V dotazu není přítomen query string!"};
+  if(!qs) {
+    var err = new Error("V dotazu není přítomen query string!");
+    err.status = 400;
+    throw err;
+  }
 
   qs = qs[0];           // extract query string part from request url
   qs = qs.substr(1);   // remove '?'
@@ -40,18 +43,25 @@ exp.validate_days = function(query) {
     var keys = Object.keys(query.filter.timestamp);
     for(var key in keys) {
       if(!(query.filter.timestamp[keys[key]].getHours() == 0 && query.filter.timestamp[keys[key]].getMinutes() == 0
-        && query.filter.timestamp[keys[key]].getSeconds() == 0 && query.filter.timestamp[keys[key]].getMilliseconds() == 0))
-        throw { error : "nesprávná specifikace intervalu!" };  // some part is set incorrectly
+        && query.filter.timestamp[keys[key]].getSeconds() == 0 && query.filter.timestamp[keys[key]].getMilliseconds() == 0)) {
+          var err = new Error("nesprávná specifikace intervalu!");  // some part is set incorrectly
+          err.status = 400;
+          throw err;
+      }
     }
   }
-  else if(query.filter.timestamp.constructor == Object && Object.keys(query.filter.timestamp).length != 2)   // more or less values
-  {
-    throw { error : "nesprávný počet hodnot proměnné timestamp pro interval!" };
+  else if(query.filter.timestamp.constructor == Object && Object.keys(query.filter.timestamp).length != 2) {  // more or less values
+    var err = new Error("nesprávný počet hodnot proměnné timestamp pro interval!");
+    err.status = 400;
+    throw err;
   }
   else {    // one day only
     if(!(query.filter.timestamp.getHours() == 0 && query.filter.timestamp.getMinutes() == 0 
-      && query.filter.timestamp.getSeconds() == 0 && query.filter.timestamp.getMilliseconds() == 0))
-      throw { error : "nesprávné datum!" };  // some part is set incorrectly
+      && query.filter.timestamp.getSeconds() == 0 && query.filter.timestamp.getMilliseconds() == 0)) {
+        var err = new Error("nesprávné datum!");  // some part is set incorrectly
+        err.status = 400;
+        throw err;
+    }
   }
 }
 // --------------------------------------------------------------------------------------
@@ -67,7 +77,9 @@ exp.validate_interval = function(query, date_req) {
       // TODO
     }
     else {
-      throw { error : "koncový timestamp musí být definován!" };
+      var err = new Error("koncový timestamp musí být definován!");
+      err.status = 400;
+      throw err;
     }
   }
 
@@ -81,14 +93,20 @@ function valid_timestamp(query)
   var keys = query.filter.timestamp;
 
   for(var key in keys)
-    if(isNaN(Date.parse(keys[key])))    // invalid date
-      throw { error : "neplatné datum: " + keys[key]};
+    if(isNaN(Date.parse(keys[key]))) {   // invalid date
+      var err = new Error("neplatné datum: " + keys[key]);
+      err.status = 400;
+      throw err;
+    }
 }
 // --------------------------------------------------------------------------------------
 function check_timestamp(query)
 {
-  if(query.filter.timestamp == undefined)
-    throw { error : "timestamp musí být definován!"};
+  if(query.filter.timestamp == undefined) {
+    var err = new Error("timestamp musí být definován!");
+    err.status = 400;
+    throw err;
+  }
 }
 // --------------------------------------------------------------------------------------
 module.exports = exp;
