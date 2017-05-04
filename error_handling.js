8889539929c2235cf651cfba1965854c6ddbf9d7
lager <machv@cesnet.cz>
2017-05-04 14:03:39 +0200

    rewrite error handling

--- error_handling.js
+++ error_handling.js
@@ -29,24 +29,15 @@ const mail = require( './mail' );
   // production error handler
   // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-     
-    var msg = err.message;
-
     if(err.status == undefined) {       // server failure
-      msg = "V aplikaci došlo k chybě. Hlášení o chybě bylo odesláno vývojářům.";
+      var msg = "V aplikaci došlo k chybě. Hlášení o chybě bylo odesláno vývojářům.";
       mail.send_error_report(err);    // send error report to developers
       res.end(msg);
     }
-    else if(err.status == 404) {
-      res.render('error', {
-        message: err.message,
-        error: {}
-      });
-    }
-    else {
-      res.end(msg);
-    }
+
+    res.status(err.status || 500);
+    var msg = err.message;
+    res.end(msg);
   });
 // --------------------------------------------------------------------------------------
 }
