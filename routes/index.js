d7906572223f363333825a846891c82ad60b602a
lager <machv@cesnet.cz>
2017-05-04 14:18:11 +0200

    add info about client sent headers

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 // --------------------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'etlog', username : req.header("REMOTE_USER") });
+  res.render('index', { title: 'etlog', username : req.header("REMOTE_USER"),
+  headers : JSON.stringify(req.headers).replace(/,/g, "\n") });
 });
 // --------------------------------------------------------------------------------------
 module.exports = router;
