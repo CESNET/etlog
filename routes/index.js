56bb3cf0ad8ccde2eb26bb01c19f208f99f1af26
lager <machv@cesnet.cz>
2017-05-20 08:50:19 +0200

    remove debug variables

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 // --------------------------------------------------------------------------------------
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'etlog', username : req.header("REMOTE_USER"),
-  headers : JSON.stringify(req.headers).replace(/,/g, "\n") });
+  res.render('index', { title: 'etlog' });
 });
 // --------------------------------------------------------------------------------------
 module.exports = router;
