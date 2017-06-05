7e8bdf974ffaac89a2feed5845b6735d68e175d8
lager <machv@cesnet.cz>
2017-06-05 08:41:26 +0200

    add realm_admin_logins collection

--- db.js
+++ db.js
@@ -26,6 +26,16 @@ exports.users_mac_schema = users_mac_schema;
 var users_mac = mongoose.model('users_mac', users_mac_schema, 'users_mac');
 exports.users_mac = users_mac;
 // --------------------------------------------------------------------------------------
+var realm_admin_logins_schema = mongoose.Schema({
+  admin : String,
+  administered_realms : Array
+},
+{ versionKey: false });
+// --------------------------------------------------------------------------------------
+exports.realm_admin_logins_schema = realm_admin_logins_schema;
+var realm_admin_logins = mongoose.model('realm_admin_logins', realm_admin_logins_schema, 'realm_admin_logins');
+exports.realm_admin_logins = realm_admin_logins;
+// --------------------------------------------------------------------------------------
 var mac_count_schema = mongoose.Schema({
   username  : String,
   count     : Number,
