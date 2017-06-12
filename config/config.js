5678a7d661cbbc8e3dfcfec9adee7e28a9756965
lager <machv@cesnet.cz>
2017-06-12 16:12:27 +0200

    enhance config

--- config/config.js
+++ config/config.js
@@ -7,6 +7,12 @@ module.exports = {
   failed_logins_lines : 100,
   // subject of failed logins report
   failed_logins_subj : "měsíční report - neúspěšná přihlášení",
+  // email of national radius admin
+  radius_admin : "jan.tomasek@cesnet.cz",
+  // from email address set for outgoing mails
+  mail_from_address: 'etlog@etlog.cesnet.cz <etlog@etlog.cesnet.cz>',
+  // reply to email address set for outgoing mails
+  mail_replyto_address: 'info@eduroam.cz',
   // ipv4 listening address
   ipv4 : '127.0.0.1',
   // ipv6 listening address
