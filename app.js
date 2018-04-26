fd41c09e89f82a3b7478729b6a35f8d52e5fd9e0
lager <machv@cesnet.cz>
2018-04-26 12:25:51 +0200

    add .well_known directory with accessible files

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/.well_known', express.static('public/.well_known'));
 
 // --------------------------------------------------------------------------------------
 // Make our db accessible to our router
