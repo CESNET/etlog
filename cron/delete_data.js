06eb7ae63a2d674705be21fd7d008b4620f538c3
lager <machv@cesnet.cz>
2018-05-24 15:03:12 +0200

    correctly access database collection

--- cron/delete_data.js
+++ cron/delete_data.js
@@ -13,7 +13,7 @@ exp.delete_old_data = function (database, collection, days) {
   var min = new Date(curr);
   min.setTime(min.getTime() - days * 86400000);     // convert days to milliseconds
   
-  database.collection(collection).remove({ timestamp : { $lt : min }});     // delete
+  database[collection].remove({ timestamp : { $lt : min }});     // delete
 };
 // --------------------------------------------------------------------------------------
 module.exports = exp;
